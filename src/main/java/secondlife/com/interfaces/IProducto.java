fe5642a0acd55b2d117be7d244e22f3ed4b2521a
madeliyricra <madeliyricra@gmail.com>
2021-05-30 21:42:33 -0500

    dependencias

--- src/main/java/secondlife/com/interfaces/IProducto.java
+++ src/main/java/secondlife/com/interfaces/IProducto.java
@@ -1,8 +1,10 @@
 package secondlife.com.interfaces;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import secondlife.com.model.Producto;
 
 
-public interface IProducto extends JpaRepository<Producto, Integer>{
+public interface IProducto extends JpaRepository<Producto, String>{
 
 }

826b121bad4b9cddf9ed74f6e01c4a1d649c1dfc
Maverick <maverick.champi@gmail.com>
2021-06-01 23:41:55 -0500

    avance registrar

---
+++ src/main/java/secondlife/com/interfaces/IUsuario.java
@@ -0,0 +1,9 @@
+package secondlife.com.interfaces;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import secondlife.com.model.Usuario;
+
+public interface IUsuario extends JpaRepository<Usuario, String>{
+
+}
